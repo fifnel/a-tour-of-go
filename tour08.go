fd1488b0a5656d1fc6085a1684fd8798d954f896
fifnel <fifnel@gmail.com>
2015-08-29 23:37:58 +0900

    add tour08

---
+++ tour08.go
@@ -0,0 +1,14 @@
+package main
+
+// Functions continued
+// http://go-tour-jp.appspot.com/#8
+
+import "fmt"
+
+func add(x, y int) int {
+	return x + y
+}
+
+func main() {
+	fmt.Println(add(111, 222))
+}
