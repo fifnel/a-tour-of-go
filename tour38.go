ab142b2146cb44e543fb2774f0c83a5a216078e0
fifnel <fifnel@gmail.com>
2015-08-31 22:52:45 +0900

    add tour38

---
+++ tour38.go
@@ -0,0 +1,23 @@
+package main
+
+// Map literals
+// http://go-tour-jp.appspot.com/#38
+
+import . "fmt"
+
+type Vertex struct {
+	Lat, Long float64
+}
+
+var m = map[string]Vertex{
+	"Bell Labs": Vertex{
+		40.68433, -74.39967,
+	},
+	"Google": Vertex{
+		37.42202, -122.08408,
+	},
+}
+
+func main() {
+	Println(m)
+}
